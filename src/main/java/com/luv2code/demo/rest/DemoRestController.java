a67a4f4fc1e2c9975aefb5b28cfd665e055a45ba
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-26 12:57:09 -0300

    First commit

---
+++ src/main/java/com/luv2code/demo/rest/DemoRestController.java
@@ -0,0 +1,13 @@
+package com.luv2code.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoRestController {
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
